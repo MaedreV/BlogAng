6ff0d2ce384cf421bfa1997fe8e3df5a8dbd4ca4
Raul Melo <raulvitor123456@hotmail.com>
2025-03-15 22:52:18 -0300

    app mudanca

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,18 @@
+export const dataFake= [
+
+
+  {
+    "id":"1",
+    "title": "Invencivel",
+    "description": "derrota conquista",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova série da Eve Atomica",
+    "description": "Disponivel na Amazon",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+
+
+]
